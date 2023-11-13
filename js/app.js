aaef7c37eb4ee0f15edd2bd8144e62d7e14e9d09
Толочко Катерина <tolochko.kateryna@gmail.com>
2023-11-13 22:29:39 +0200

    Add html, css and js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function imgSlider(path) {
+  document.querySelector(".starbucks").src = path;
+}
+
+function changeCircleColor(color) {
+  document.querySelector(".circle").style.background = color;
+}
